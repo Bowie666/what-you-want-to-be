6a3cb4da086fa04ca8356a7054a6d0c4faba5aef
徐威 <ldldxw@163.com>
2020-04-07 13:25:12 +0800

    查看内存使用率 总内存 可用内存

--- profeskills/backend/python/需要整理.py
+++ profeskills/backend/python/需要整理.py
@@ -8,3 +8,17 @@ git merge 5km
 git commit
 在merge就好
 """
+import psutil
+import time
+
+# 查看内存
+mem = psutil.virtual_memory()
+total = str(round(mem.total / 1024 / 1024))
+
+# round方法进行四舍五入，然后转换成字符串 字节/1024得到kb 再/1024得到M
+used = str(round(mem.used / 1024 / 1024))
+use_per = str(round(mem.percent))
+free = str(round(mem.free / 1024 / 1024))
+print("您当前的内存大小为:" + total + "M")  # 您当前的内存大小为:8076M
+print("已使用:" + used + "M(" + use_per + "%)")  # 已使用:5805M(72%)
+print("可用内存:" + free + "M")  # 可用内存:2271M
