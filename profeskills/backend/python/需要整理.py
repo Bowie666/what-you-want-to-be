380b4d532380c13de871e55062b261b58203c05a
徐威 <ldldxw@163.com>
2020-04-07 17:51:51 +0800

    查看GPU参数

--- profeskills/backend/python/需要整理.py
+++ profeskills/backend/python/需要整理.py
@@ -36,3 +36,34 @@ print(str(psutil.disk_usage("/").total / 1024 / 1024))
 psutil.disk_partitions()  # 磁盘分区信息 [sdiskpart(device='/dev/disk1', mountpoint='/', fstype='hfs', opts='rw,local,rootfs,dovolfs,journaled,multilabel')]
 psutil.disk_usage('/')  # 磁盘使用情况 sdiskusage(total=998982549504, used=390880133120, free=607840272384, percent=39.1)
 psutil.disk_io_counters()  # 磁盘IO sdiskio(read_count=988513, write_count=274457, read_bytes=14856830464, write_bytes=17509420032, read_time=2228966, write_time=1618405)
+
+# 查看显卡, 放到服务器上使用
+# 简单使用
+from pynvml import *
+
+nvmlInit()  # 初始化
+print("Driver: ", nvmlSystemGetDriverVersion())  # 显示驱动信息
+# >>> Driver: 384.xxx
+
+# 查看设备
+deviceCount = nvmlDeviceGetCount()
+for i in range(deviceCount):
+    handle = nvmlDeviceGetHandleByIndex(i)
+    print("GPU", i, ":", nvmlDeviceGetName(handle))
+# >>>
+# GPU 0 : b'GeForce GTX 1080 Ti'
+# GPU 1 : b'GeForce GTX 1080 Ti'
+
+# 查看显存、温度、风扇、电源
+handle = nvmlDeviceGetHandleByIndex(0)
+info = nvmlDeviceGetMemoryInfo(handle)
+print("Memory Total: ", info.total)  # 第二块显卡总的显存大小
+print("Memory Free: ", info.free)  # 这里是字节bytes，所以要想得到以兆M为单位就需要除以1024**2
+print("Memory Used: ", info.used)  # 第二块显卡剩余显存大小
+
+print("Temperature is %d C" % nvmlDeviceGetTemperature(handle, 0))
+print("Fan speed is ", nvmlDeviceGetFanSpeed(handle))
+print("Power ststus", nvmlDeviceGetPowerState(handle))
+
+# 最后要关闭管理工具
+nvmlShutdown()
