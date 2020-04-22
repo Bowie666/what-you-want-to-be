94b885b361ed4eeafb4d25bc2fe186f4b309c09c
徐威 <ldldxw@163.com>
2020-04-22 14:09:23 +0800

    max 方法整理

--- profeskills/backend/python/stdlib/1.Built-inFunctions.py
+++ profeskills/backend/python/stdlib/1.Built-inFunctions.py
@@ -1,3 +1,17 @@
+"""41 max(iterable, *[, key, default])
+max(arg1, arg2, *args[, key])
+返回可迭代对象中最大的元素，或者返回两个及以上实参中最大的。
+如果只提供了一个位置参数，它必须是非空 iterable，返回可迭代对象中最大的元素；
+如果提供了两个及以上的位置参数，则返回最大的位置参数。
+有两个可选只能用关键字的实参。key 实参指定排序函数用的参数，如传给 list.sort() 的。
+default 实参是当可迭代对象为空时返回的值。如果可迭代对象为空，并且没有给 default ，则会触发 ValueError。
+如果有多个最大元素，则此函数将返回第一个找到的。这和其他稳定排序工具如:
+sorted(iterable, key=keyfunc, reverse=True)[0] 和 heapq.nlargest(1, iterable, key=keyfunc) 保持一致。"""
+# print("max(80, 100, 1000) : ", max([80, 100, 1000], [80, 100, 1001, 55]))  # max(80, 100, 1000) :  [80, 100, 1001, 55]
+# print("max(-20, 100, 400) : ", max(-20, 100, 400))  # max(-20, 100, 400) :  400
+# print("max(-80, -20, -10) : ", max(-80, -20, -10))  # max(-80, -20, -10) :  -10
+# print("max(0, 100, -400) : ", max(0, 100, -400))  # max(0, 100, -400) :  100
+
 """40 map(function, iterable, ...)   function -- 函数  iterable -- 一个或多个序列
 返回一个将 function 应用于 iterable 中每一项并输出其结果的迭代器。
 如果传入了额外的 iterable 参数，function 必须接受相同个数的实参并被应用于从所有可迭代对象中并行获取的项。
