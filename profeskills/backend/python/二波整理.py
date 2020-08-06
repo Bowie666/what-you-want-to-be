512cea917825611a7678617e2a2e789db5ea7715
徐威 <ldldxw@163.com>
2020-08-06 17:31:54 +0800

    脚本连接pymongo

--- profeskills/backend/python/二波整理.py
+++ profeskills/backend/python/二波整理.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from Crypto.Cipher import AES
+# from Crypto.Cipher import AES
 import base64
 import time
 import gzip
@@ -12,71 +12,104 @@ from xml.dom import minidom
 import shutil
 import json
 
-# -----------------gif 动图分解-------------
-import os
-from PIL import Image
-def analyseImage(path):
-  '''
-  Pre-process pass over the image to determine the mode (full or additive).
-  Necessary as assessing single frames isn't reliable. Need to know the mode
-  before processing all frames.
-  '''
-  im = Image.open(path)
-  results = {
-    'size': im.size,
-    'mode': 'full',
-  }
-  try:
-    while True:
-      if im.tile:
-        tile = im.tile[0]
-        update_region = tile[1]
-        update_region_dimensions = update_region[2:]
-        if update_region_dimensions != im.size:
-          results['mode'] = 'partial'
-          break
-      im.seek(im.tell() + 1)
-  except EOFError:
-    pass
-  return results
+
+# ------------------ Python 连接 MongoDB -------------------
+#导入模块
+from pymongo import MongoClient
+#建立Mongodb数据库连接
+client=MongoClient('localhost',27017)
+#test为数据库
+db=client.app
+# 认证
+db.authenticate("tian", "123")
+#test为集合，相当于表名(没有的话会去创建的)
+collection=db.address
+#插入集合数据
+# collection.insert({"title":"test"})
+# 更新某个字段
+student = collection.find_one({'name': 'hu'})
+student['address'] = 'button'
+result = collection.update_one({'name': 'hu'}, {'$set': student})
+#打印集合中所有数据
+for item in collection.find():
+    print(item)
+#更新集合里的数据
+# collection.update({"title":"test"},{"title":"this is update test"})
+#关闭连接
+client.close()
+#!!!!其他操作
+#查找集合中单条数据
+#print collection.find_one()
+#删除集合collection中的所有数据
+#collection.remove()
+#删除集合collection
+#collection.drop()
+
+# # -----------------gif 动图分解 暂时还没测试-------------
+# import os
+# from PIL import Image
+# def analyseImage(path):
+#   '''
+#   Pre-process pass over the image to determine the mode (full or additive).
+#   Necessary as assessing single frames isn't reliable. Need to know the mode
+#   before processing all frames.
+#   '''
+#   im = Image.open(path)
+#   results = {
+#     'size': im.size,
+#     'mode': 'full',
+#   }
+#   try:
+#     while True:
+#       if im.tile:
+#         tile = im.tile[0]
+#         update_region = tile[1]
+#         update_region_dimensions = update_region[2:]
+#         if update_region_dimensions != im.size:
+#           results['mode'] = 'partial'
+#           break
+#       im.seek(im.tell() + 1)
+#   except EOFError:
+#     pass
+#   return results
 
 
-def processImage(path):
-  '''
-  Iterate the GIF, extracting each frame.
-  '''
-  mode = analyseImage(path)['mode']
-  im = Image.open(path)
-  i = 0
-  p = im.getpalette()
-  last_frame = im.convert('RGBA')
-  try:
-    while True:
-      print("saving %s (%s) frame %d, %s %s") % (path, mode, i, im.size, im.tile)
-      '''
-      If the GIF uses local colour tables, each frame will have its own palette.
-      If not, we need to apply the global palette to the new frame.
-      '''
-      if not im.getpalette():
-        im.putpalette(p)
-      new_frame = Image.new('RGBA', im.size)
-      '''
-      Is this file a "partial"-mode GIF where frames update a region of a different size to the entire image?
-      If so, we need to construct the new frame by pasting it on top of the preceding frames.
-      '''
-      if mode == 'partial':
-        new_frame.paste(last_frame)
-      new_frame.paste(im, (0,0), im.convert('RGBA'))
-      new_frame.save('%s-%d.png' % (''.join(os.path.basename(path).split('.')[:-1]), i), 'PNG')
-      i += 1
-      last_frame = new_frame
-      im.seek(im.tell() + 1)
-  except EOFError:
-    pass
-def main():
-  processImage(r'C:\Users\Administrator\Desktop\333.gif')
-if __name__ == "__main__":
-  main()
+# def processImage(path):
+#   '''
+#   Iterate the GIF, extracting each frame.
+#   '''
+#   mode = analyseImage(path)['mode']
+#   im = Image.open(path)
+#   i = 0
+#   p = im.getpalette()
+#   last_frame = im.convert('RGBA')
+#   try:
+#     while True:
+#       print("saving %s (%s) frame %d, %s %s") % (path, mode, i, im.size, im.tile)
+#       '''
+#       If the GIF uses local colour tables, each frame will have its own palette.
+#       If not, we need to apply the global palette to the new frame.
+#       '''
+#       if not im.getpalette():
+#         im.putpalette(p)
+#       new_frame = Image.new('RGBA', im.size)
+#       '''
+#       Is this file a "partial"-mode GIF where frames update a region of a different size to the entire image?
+#       If so, we need to construct the new frame by pasting it on top of the preceding frames.
+#       '''
+#       if mode == 'partial':
+#         new_frame.paste(last_frame)
+#       new_frame.paste(im, (0,0), im.convert('RGBA'))
+#       new_frame.save('%s-%d.png' % (''.join(os.path.basename(path).split('.')[:-1]), i), 'PNG')
+#       i += 1
+#       last_frame = new_frame
+#       im.seek(im.tell() + 1)
+#   except EOFError:
+#     pass
+# def main():
+#   processImage(r'C:\Users\Administrator\Desktop\333.gif')
+# if __name__ == "__main__":
+#   main()
 
 # ------------------图片的二进制编码---------------
 # img = b"C:\Users\Administrator\Desktop\111\\1.png"
