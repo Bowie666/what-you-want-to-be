48dd578fc36de3876ea6693e9ac97c654715fffb
徐威 <ldldxw@163.com>
2021-01-16 16:11:43 +0800

    3.6的版本所有的关键字

---
+++ profeskills/backend/python/stdlib/keyword.py
@@ -0,0 +1,11 @@
+#导入keyword 模块
+import keyword
+#显示所有关键字
+# print(keyword.kwlist)
+
+"""python 3.6 版本
+['False', 'None', 'True', 'and', 'as', 'assert', 'break', 'class', 
+'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 
+'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 
+'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+"""
