8d63d8340198618894500f9359e1084073f884b2
徐威 <ldldxw@163.com>
2021-01-18 13:45:43 +0800

    as class def del if elif else try except finally

--- profeskills/backend/python/stdlib/keyword.py
+++ profeskills/backend/python/stdlib/keyword.py
@@ -10,11 +10,45 @@ import keyword
 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
 """
 
-# 4 break contiue 
+# 10 try except else finally
+# 有异常执行 except 没异常执行 else 不管有没有异常执行 finally
+
+# 9 if elif else
+# 配合使用
+# a = 1
+# if a == 1:
+#     print(a)
+# elif a == 2:
+#     print(a)
+# else:
+#     print(a)
+
+# 8 del
+# 删除字典的键
+# 或者是删除变了
+# a = 1
+# print(a)  # 1
+# del a
+# print(a)  # NameError: name 'a' is not defined
+
+# 7 def
+# 用于定义函数
+
+# 6 class
+# 只可意会不可言传的东西
+# 用于定义类
+
+# 5 as
+# 给我的感觉就是重命名
+# import numpy as np
+# with open('file') as f:
+#    pass
+
+# 4 break contiue
 # break 用来终止循环 用在while和for循环中 直接跳出整个循环
 # continue 是跳出当前循环
 
-# 3 assert 
+# 3 assert
 # 声明其布尔值必须为真的判定 如果发生异常就说明表达示为假
 # assert 1 == 2  # AssertionError
 # assert 1 == 1
